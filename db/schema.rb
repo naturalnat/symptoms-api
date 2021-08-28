8b83d766a7fefa28de430f4fe0fee924f935b47f
Nat <ntaktach@gmail.com>
2021-08-28 14:22:18 -0400

    make heroku ready pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_170728) do
+ActiveRecord::Schema.define(version: 2021_08_22_030741) do
 
   create_table "symptoms", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_08_04_170728) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "severity"
     t.boolean "flag", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "User"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
